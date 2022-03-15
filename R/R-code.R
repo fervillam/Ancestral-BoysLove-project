9eafc002e1071dff04331003f9c84add4094c8d9
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-03-15 16:27:48 -0500

    Create R-code.R

---
+++ R/R-code.R
@@ -0,0 +1,9 @@
+if(!require("ape")){install.packages("ape")} #Installing "ape" package
+if(!require("ape")){install.packages("rexpokit")}
+if(!require("ape")){install.packages("cladoRcpp")}
+if(!require("ape")){install.packages("devtools", dependencies = TRUE)}
+
+library(ape)
+library(rexpokit)
+library(cladoRcpp)
+library(devtools)
