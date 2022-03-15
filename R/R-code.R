f694e8fd0463d35c7447a4bbca1ef934931ed8e9
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-03-15 16:33:12 -0500

    Update R-code.R

--- R/R-code.R
+++ R/R-code.R
@@ -1,4 +1,6 @@
-if(!require("ape")){install.packages("ape")} #Installing "ape" package
+
+#Installing required packages
+if(!require("ape")){install.packages("ape")}
 if(!require("ape")){install.packages("rexpokit")}
 if(!require("ape")){install.packages("cladoRcpp")}
 if(!require("ape")){install.packages("devtools", dependencies = TRUE)}
@@ -7,3 +9,5 @@ library(ape)
 library(rexpokit)
 library(cladoRcpp)
 library(devtools)
+
+devtools::install_github(repo="nmatzke/BioGeoBEARS", INSTALL_opts="--byte-compile")
