95a9e035d1701d7d38e2d795007fad2d1277e132
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-03-15 20:11:28 -0500

    Update Boys-Love.sh

--- Boys-Love.sh
+++ Boys-Love.sh
@@ -6,9 +6,6 @@ sudo apt-get install libcurl4-openssl-dev
 sudo apt-get install libssl-dev
 
 R #Run R console
-  if(!require("ape")){install.packages("ape")} #Installing "ape" package
-  if(!require("ape")){install.packages("rexpokit")}
-  if(!require("ape")){install.packages("cladoRcpp")}
-  if(!require("ape")){install.packages("devtools", dependencies = TRUE)}
+  Rscript Ancestral-areas-R
   
   q() #Exit R console and returning to bash
