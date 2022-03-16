e16bba7af15548063d5843624b431820c554b5d9
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-03-16 16:01:46 -0500

    Update Boys-Love.sh

--- Boys-Love.sh
+++ Boys-Love.sh
@@ -4,6 +4,8 @@ cd Documents/Ancestral-BoysLove-Project #Setting work directory
 sudo apt-get install libxml2-dev
 sudo apt-get install libcurl4-openssl-dev
 sudo apt-get install libssl-dev
+sudo apt install python2
+
 
 R #Run R console
   Rscript Ancestral-areas-R
