8153ec35890545095d965d3528bf76fbfc42d0d8
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-03-15 22:17:31 -0500

    Update Ancestral-Areas.R

--- R/Ancestral-Areas.R
+++ R/Ancestral-Areas.R
@@ -3,13 +3,16 @@ setwd(getwd())
 
 #Installing required packages
 if(!require("ape")){install.packages("ape")}
-if(!require("ape")){install.packages("rexpokit")}
-if(!require("ape")){install.packages("cladoRcpp")}
-if(!require("ape")){install.packages("devtools", dependencies = TRUE)}
+if(!require("rexpokit")){install.packages("rexpokit")}
+if(!require("cladoRcpp")){install.packages("cladoRcpp")}
+if(!require("GenSA")){install.packages("GenSA")}
+if(!require("devtools")){install.packages("devtools", dependencies = TRUE)}
 
 library(ape)
 library(rexpokit)
 library(cladoRcpp)
+library(GenSA)
 library(devtools)
 
 devtools::install_github(repo="nmatzke/BioGeoBEARS", INSTALL_opts="--byte-compile")
+
