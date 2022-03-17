c67cc0658666798804b44eb0c5fb95b0425833c6
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-03-16 20:01:48 -0500

    Update Boys-Love.sh

--- Boys-Love.sh
+++ Boys-Love.sh
@@ -5,6 +5,13 @@ sudo apt-get install libxml2-dev
 sudo apt-get install libcurl4-openssl-dev
 sudo apt-get install libssl-dev
 sudo apt install python2
+sudo apt update
+sudo apt install curl
+sudo add-apt-repository universe
+sudo apt update
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py # Fetch get-pip.py for python 2.7 
+python2 get-pip.py
+python2 -m pip install lagrange
 
 
 R #Run R console
