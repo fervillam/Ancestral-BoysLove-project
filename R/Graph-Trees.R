073541a0f9276445dce2292c419d3e0a03560e76
Fernando A. Villamizar-Moreno <fervillam07@gmail.com>
2022-03-21 17:44:51 -0500

    Update Graph-Trees.R

--- R/Graph-Trees.R
+++ R/Graph-Trees.R
@@ -6,6 +6,6 @@ library(Rcpp)
 
 png(file = 'Equal-BL-Trees.png')
 par(mfrow=c(1,2))
-plot.phylo(eq1, main = "2x2 Tree with equal BL"); nodelabels()
-plot.phylo(eq2, main = "3x1 Tree with equal BL"); nodelabels()
+plot.phylo(eq1, main = "a. 2x2 Tree with equal BL"); nodelabels()
+plot.phylo(eq2, main = "b. 3x1 Tree with equal BL"); nodelabels()
 dev.off()
